220f0e0d041ca8db6fefd4e53491c81dd17df9e8
reviverknight <91496259+reviverknight@users.noreply.github.com>
2021-09-28 11:47:22 +0100

    V1

---
+++ js/rk.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+     $(".videoBox").fitVids();
+});
+
+var colors = ["#ff5c50", "#8981f1", "9b22c5"];
+var color = colors[Math.floor(Math.random() * colors.length)];
+
+
+
+$('.item').mouseover(function() {
+   $('.overlay').css("background", color); // change css
+});
+$('.item').mouseleave(function() {
+   color = colors[Math.floor(Math.random() * colors.length)];
+});
+
